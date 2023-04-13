b1e77e5e6516e15d1260dd7a3e51442ac9ed8363
Egor Villanueva <eviespi@gmail.com>
2023-04-12 23:49:06 -0500

    Fin seccion 10 - Auth

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,24 +6,27 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
-const { getUsuarios, crearUsuario, actualizarUsuario } = require('../controllers/usuarios');
+const { getUsuarios, crearUsuario, actualizarUsuario, eliminarUsuario } = require('../controllers/usuarios');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
-router.get('/', getUsuarios);
+router.get('/', validarJWT, getUsuarios);
 
-router.post('/', [
+router.post('/', validarJWT, [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password es obligatorio').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
     validarCampos
 ], crearUsuario);
 
-router.put('/:id', [
+router.put('/:id', validarJWT, [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
     check('role', 'El rol es obligatorio').not().isEmpty(),
     validarCampos
 ], actualizarUsuario);
+
+router.delete('/:id', validarJWT, eliminarUsuario);
 
 module.exports = router;
